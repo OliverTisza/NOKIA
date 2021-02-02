06ae4ac94781533f92cc56699932b4c7fc5aa626
OliverTisza <36072634+OliverTisza@users.noreply.github.com>
2021-02-02 15:58:47 +0100

    MVP
    - Summary:
    -- Move (A and D)
    -- Jump (W)
    -- Toggle Platform Colour (Space)
    -- The Boxes can stand on the same coloured platform and fall through the opposite coloured

---
+++ Nokia GameJam/Assets/Scripts/PlatformToggler.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformToggler : MonoBehaviour
+{
+
+    GameObject[] whitePlatforms;
+    GameObject[] blackPlatforms;
+    // Start is called before the first frame update
+    void Start()
+    {
+        whitePlatforms = GameObject.FindGameObjectsWithTag("White");
+        blackPlatforms = GameObject.FindGameObjectsWithTag("Black");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            TogglePlatforms();
+        }
+    }
+
+    void TogglePlatforms()
+    {
+        foreach (var platform in whitePlatforms)
+        {
+            if (platform.GetComponent<SpriteRenderer>())
+            {
+                platform.GetComponent<BoxCollider2D>().enabled = !platform.GetComponent<BoxCollider2D>().enabled;
+                platform.GetComponent<SpriteRenderer>().enabled = !platform.GetComponent<SpriteRenderer>().enabled;
+                //platform.tag = "Black";
+            }
+            
+        }
+
+        foreach (var platform in blackPlatforms)
+        {
+            if (platform.GetComponent<SpriteRenderer>())
+            {
+                platform.GetComponent<BoxCollider2D>().enabled = !platform.GetComponent<BoxCollider2D>().enabled;
+                platform.GetComponent<SpriteRenderer>().enabled = !platform.GetComponent<SpriteRenderer>().enabled;
+                //platform.tag = "White";
+            }
+                
+        }
+
+        /*
+        whitePlatforms = GameObject.FindGameObjectsWithTag("White");
+        blackPlatforms = GameObject.FindGameObjectsWithTag("Black");
+        */
+    }
+}
--- Nokia GameJam/Assets/Scripts/PlayerMovement.cs
+++ Nokia GameJam/Assets/Scripts/PlayerMovement.cs
@@ -17,14 +17,14 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(Input.GetAxis("Horizontal"));
+        
         gameObject.transform.position += new Vector3(Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime, 0, 0);
         if(Input.GetAxis("Horizontal") < 1.0f)
         {
             gameObject.transform.position += new Vector3(0, 0, 0);
         }
         
-        Debug.Log(Input.GetAxis("Vertical"));
+        
         if (gameObject.GetComponent<Rigidbody2D>() && Input.GetAxis("Vertical") > 0.0f && !jumping)
         {
             gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0, jumpForce));
@@ -39,6 +39,22 @@ public class PlayerMovement : MonoBehaviour
     private void OnCollisionEnter2D(Collision2D collision)
     {
         jumping = false;
+
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log(collision.gameObject.tag);
+        if (collision.gameObject.tag == "Untagged")
+        {
+
+        }
+        else if (collision.gameObject.tag != gameObject.tag)
+        {
+            
+            collision.gameObject.GetComponent<BoxCollider2D>().enabled = false;
+        }
     }
 
 
