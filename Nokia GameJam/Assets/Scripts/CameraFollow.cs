8fcc2d694a45983389d7f4416d181616ca50eb83
Borda Ádám <h749661@stud.u-szeged.hu>
2021-02-04 21:14:09 +0100

    Camera clamping added & little tweaking on map

--- Nokia GameJam/Assets/Scripts/CameraFollow.cs
+++ Nokia GameJam/Assets/Scripts/CameraFollow.cs
@@ -4,45 +4,15 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
-
     public Transform player;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
+    [Header("Clamping Options")]
+    [SerializeField] private float minY;
+    [SerializeField] private float maxY;
+
     void Update()
     {
-        if(gameObject.transform.position.y > 0)
-        {
-            
-        }
-
-        gameObject.transform.position = new Vector3(gameObject.transform.position.x, player.transform.position.y, gameObject.transform.position.z);
-
+        transform.position = new Vector3(transform.position.x, Mathf.Clamp(player.position.y, minY, maxY), transform.position.z);
     }
-
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        //MOVE CAMERA ON Y AXIS
-        /*
-        if (collision.name.Contains("Player") && collision.GetComponent<Rigidbody2D>().velocity.y > 0.0f)
-        {
-            
-
-            gameObject.transform.position = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y + 24, gameObject.transform.position.z);
-        }
-        else if (collision.name.Contains("Player") && collision.GetComponent<Rigidbody2D>().velocity.y < 0.0f)
-        {
-            gameObject.transform.position = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y - 24, gameObject.transform.position.z);
-        }
-        */
-
-
-
-    }
-
 
 }
