92a21e54e7b83a98f847f4056ea471ce87b3af64
Borda Ádám <h749661@stud.u-szeged.hu>
2021-02-07 14:56:31 +0100

    main menu fixed

--- Nokia GameJam/Assets/Scripts/MainMenu.cs
+++ Nokia GameJam/Assets/Scripts/MainMenu.cs
@@ -7,9 +7,17 @@ using UnityEngine.UI;
 public class MainMenu : MonoBehaviour
 {
 
-    public RectTransform selector;
     public Button[] buttons;
     public Text tutorialText;
+
+    private int activeIndex = 0;
+    private Vector2 highlightedVector = new Vector2(5, -5);
+    private Vector2 outlinedVector = new Vector2(3, -3);
+
+    // 0 - Start
+    // 1 - Tutorial
+    // 2 - Exit
+
     // Start is called before the first frame update
     void Start()
     {
@@ -18,6 +26,8 @@ public class MainMenu : MonoBehaviour
         {
             button.gameObject.SetActive(true);
         }
+
+        buttons[0].GetComponent<Outline>().effectDistance = highlightedVector;
     }
 
     // Update is called once per frame
@@ -26,29 +36,23 @@ public class MainMenu : MonoBehaviour
 
         if (Input.GetKeyDown(KeyCode.W))
         {
-            
-            if(selector.transform.position.y > 26)
-            {
-                selector.anchoredPosition -= new Vector2(0,26);
-            } else
-            {
-                selector.anchoredPosition += new Vector2(0, 13);
-            }
+            SetButtonsToDefault();
+            activeIndex--;
+            if (activeIndex < 0) activeIndex = 2;
+            buttons[activeIndex].GetComponent<Outline>().effectDistance = highlightedVector;
+
+
         }
         else if (Input.GetKeyDown(KeyCode.S))
         {
-            if (selector.anchoredPosition.y >= 0)
-            {
-                selector.anchoredPosition -= new Vector2(0, 13);
-            }
-            else
-            {
-                selector.anchoredPosition += new Vector2(0, 26); 
-            }
+            SetButtonsToDefault();
+            activeIndex++;
+            if (activeIndex > 2) activeIndex = 0;
+            buttons[activeIndex].GetComponent<Outline>().effectDistance = highlightedVector;
         }
         else if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.Return))
         {
-            int case_switch = (int)selector.anchoredPosition.y;
+            /*int case_switch = (int)selector.anchoredPosition.y;
 
             switch (case_switch)
             {
@@ -77,12 +81,33 @@ public class MainMenu : MonoBehaviour
 
                 default:
                     break;
+            }*/
+
+            switch(activeIndex)
+            {
+                case 0:
+                    Debug.Log("Start level 1");
+                    SceneManager.LoadScene("Level1");
+                    break;
+                case 1:
+                    Debug.Log("Show Tutorial");
+
+                    tutorialText.gameObject.SetActive(true);
+                    foreach (var button in buttons)
+                    {
+                        button.gameObject.SetActive(false); ;
+                    }
+                    break;
+                case 2:
+                    Application.Quit();
+                    Debug.Log("Quit");
+                    break;
             }
+
         }
         else if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.Escape))
         {
             tutorialText.gameObject.SetActive(false);
-            selector.gameObject.SetActive(true);
             foreach (var button in buttons)
             {
                 button.gameObject.SetActive(true); ;
@@ -92,5 +117,13 @@ public class MainMenu : MonoBehaviour
         
         
         
+    }
+
+    private void SetButtonsToDefault()
+    {
+        foreach(var b in buttons)
+        {
+            b.GetComponent<Outline>().effectDistance = outlinedVector;
+        }
     }
 }
